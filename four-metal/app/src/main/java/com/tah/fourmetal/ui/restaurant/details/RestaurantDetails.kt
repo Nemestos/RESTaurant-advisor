68d0082e0cc21f19e774b631f0f0974afabf0dfb
turpin_l <turpin_l@etna-alternance.net>
2022-03-04 11:06:13 +0100

    [ADD]clickable url for restaurant

--- four-metal/app/src/main/java/com/tah/fourmetal/ui/restaurant/details/RestaurantDetails.kt
+++ four-metal/app/src/main/java/com/tah/fourmetal/ui/restaurant/details/RestaurantDetails.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Card
@@ -22,9 +23,13 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
@@ -107,9 +112,7 @@ private fun RestaurantDetailInfos(restaurant: Restaurant?) {
         fontFamily = Roboto,
         overflow = TextOverflow.Ellipsis
     )
-    Text(
-        text = restaurant?.website.orEmpty(),
-    )
+    ClickableUrl(url = restaurant?.website.orEmpty(), text = "our website")
     Text(
         text = restaurant?.phone_number.orEmpty(),
         fontFamily = Roboto,
@@ -138,6 +141,21 @@ private fun RestaurantDetailInfos(restaurant: Restaurant?) {
 
         }
     }
+}
+
+@Composable
+fun ClickableUrl(url: String, text: String) {
+    val uriHandler = LocalUriHandler.current
+    val annotatedString = buildAnnotatedString {
+        pushStringAnnotation("website", annotation = url)
+        withStyle(style = SpanStyle(color = Color.Blue)) {
+            append(text)
+        }
+        pop()
+    }
+    ClickableText(text = annotatedString, style = MaterialTheme.typography.body1, onClick = {
+        uriHandler.openUri(url)
+    })
 }
 
 @Composable
